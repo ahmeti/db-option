2453fc71d77d17989119ac71e416dfa247c10ab0
Ahmet Imamoglu <ahmet363@gmail.com>
2018-03-28 12:02:00 +0300

    Added Get and Set methods.

--- src/Services/DBOption.php
+++ src/Services/DBOption.php
@@ -2,10 +2,55 @@
 
 namespace Ahmeti\DBOption\Services;
 
+use Illuminate\Support\Facades\DB;
+
 class DBOption {
 
-    public function test()
+    public function get($name)
     {
+        $option = DB::table('options')
+            ->where('name', $name)
+            ->first();
 
+        if($option){
+
+            if ($option->type === 'int'){
+                return (int)$option->value;
+
+            }elseif ($option->type === 'float'){
+                return (float)$option->value;
+
+            }elseif ($option->type === 'json'){
+                return json_decode($option->value);
+            }
+
+            return (string)$option->value;
+        }
+
+        return null;
+    }
+
+    public function set($name, $value, $type='string', $description=null)
+    {
+        $option = DB::table('options')
+            ->where('name', $name)
+            ->first();
+
+        if($option){
+            return DB::table('options')
+                ->where('name', $name)
+                ->update([
+                    'type' => $type,
+                    'value' => $value,
+                    'description' => $description
+                ]);
+        }
+
+        return DB::table('options')->insert([
+            'name' => $name,
+            'type' => $type,
+            'value' => $value,
+            'description' => $description
+        ]);
     }
 }
